91437c8de907f7bf26993b1bbe6c8a98fda8c5a5
Salil Wadnerkar <rohshall@gmail.com>
2012-12-01 21:54:42 +0800

    parsed the json feed and displayed articles

--- hnews.cpp
+++ hnews.cpp
@@ -1,7 +1,7 @@
 #include <curl/curl.h>
 #include <string>
 #include <iostream>
-#include <json/json.h>
+#include <jansson.h>
 
 /*
  * Write data callback function (called within the context of 
@@ -20,9 +20,34 @@ size_t write_data( void *buffer, size_t size, size_t nmemb, void *userp )
 
 void hnews_parse( std::string feed )
 {
-  json_object * jobj = json_tokener_parse( feed.c_str() );
-  json_type type = json_object_get_type( jobj );
-  std::cout << "Got JSON " << type << std::endl;
+  json_error_t j_error;
+  const json_t *root = json_loads( feed.c_str(), 0, &j_error );
+  if ( !root ) {
+    std::cerr << "Unexpected JSON response: error " << j_error.text << std::endl;
+  } else {
+    const json_t *j_array = json_object_get( root, "items" );
+    size_t j_array_size = json_array_size( j_array );
+    for( size_t i = 0; i < j_array_size-1; i++ ) {
+      const json_t *j_article = json_array_get( j_array, i );
+      json_t *j_title = json_object_get( j_article, "title" );
+      std::cout << i << ": " << json_string_value( j_title ) << std::endl;
+      json_t *j_url = json_object_get( j_article, "url" );
+      std::cout << json_string_value( j_url ) << std::endl;
+      json_t *j_comments_count = json_object_get( j_article, "comments" );
+      if( j_comments_count != NULL ) {
+        std::cout << " (" << json_string_value( j_comments_count ) << ")";
+      }
+      json_t *j_score = json_object_get( j_article, "score" );
+      if( j_score != NULL ) {
+        std::cout << " (" << json_string_value( j_score ) << ")";
+      }
+      if( j_comments_count || j_score ) {
+        std::cout << std::endl;
+      }
+      std::cout << std::endl;
+    }
+  }
+  std::cout.flush();
 }
  
 
